052a24607ab19b2e909edccf467e0006faaa62be
Andre Muehle <andre.muehle@migrosluzern.ch>
2018-04-11 15:52:57 +0200

    changed to scss and connect to firebase backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  items: Observable<{}[]>;
+
+  constructor(private firebase: AngularFireDatabase) {
+    this.items = this.firebase.list('items').valueChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDYm_INLLwSPRQmZ2jEsDdSLRX6o2vNU1g',
+  authDomain: 'mue-shoplist.firebaseapp.com',
+  databaseURL: 'https://mue-shoplist.firebaseio.com',
+  projectId: 'mue-shoplist',
+  storageBucket: 'mue-shoplist.appspot.com',
+  messagingSenderId: '689727068064'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
