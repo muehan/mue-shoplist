2cfc7c55efd0200b595c77776a52eee30b98b087
Andre Muehle <andre.muehle@migrosluzern.ch>
2018-04-18 15:36:50 +0200

    login formcontrol changed

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'mue-login',
@@ -12,29 +13,31 @@ import { Router } from '@angular/router';
 
 export class MueLoginComponent implements OnInit {
 
-  public mail: string;
   public password: string;
   public errorMessage: string;
+
+  public mailFormControl: FormControl = new FormControl();
 
   constructor(
     private authService: AuthService,
     public router: Router
-  ) { }
+  ) {
+    let storedMail = localStorage.getItem('mail');
+    console.log(storedMail);
+    if (storedMail || storedMail !== undefined) {
+      this.mailFormControl.setValue(storedMail);
+    }
+  }
 
   ngOnInit() {
     if (this.authService.authenticated) {
       this.router.navigate(['list']);
     }
-
-    let storedMail = localStorage.getItem('mail');
-    if (storedMail) {
-      this.mail = storedMail;
-    }
   }
 
   login() {
-    localStorage.setItem('mail', this.mail);
-    this.authService.emailLogin(this.mail, this.password)
+    localStorage.setItem('mail', this.mailFormControl.value);
+    this.authService.emailLogin(this.mailFormControl.value, this.password)
       .then((_) => {
         this.router.navigate(['list']);
       })
